509d62b91b7d5abe91e965ed4f5a82e9cc4025a5
anthoox <kray.a4@gmail.com>
2023-03-26 14:01:17 +0200

    Elimado comentario de config.php

--- dev/mvc/config/config.php
+++ dev/mvc/config/config.php
@@ -1,6 +1,6 @@
 <?php 
 /**Constantes para la conexión con el servidor y demás que usemos en la aplicación*/
-define('DB_HOST', 'localhost');//esta configuración debe cambiar cuando este en la versión pública
+define('DB_HOST', 'localhost');
 define('DB', 'bd_lista_simple');
 define('DB_USER', 'ls_admin');
 define('DB_PASS', 'M_8d(aLK9-]6zxgZ');
