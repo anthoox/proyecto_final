d7d11b8d5e414708dfeba8c881099367c25f5410
anthoox <kray.a4@gmail.com>
2023-04-23 17:02:53 +0200

    el menu pasa de section a nav y se añade el nombre del usuario

--- dev/mvc/views/users/menu.php
+++ dev/mvc/views/users/menu.php
@@ -1,10 +1,10 @@
 <?php
 echo'
-<section class=" border bg-white ps-2 pe-2 shadow menu">
+<nav class=" border bg-white ps-2 pe-2 shadow menu">
     <ul class="list-group list-group-flush">
         <li class="d-flex flex-column justify-content-center align-items-center list-group-item list-group-item-action">
             <img class="rounded-circle" src="/proyecto/dev/mvc/resources/img/img-users/img-user.png" alt="imagen de usuario">
-            <a href="./../users/user_profile.php" class="mt-1 fs-5 text-decoration-none text-black">Perfil de usuario</a>
+            <a href="./../users/user_profile.php" class="mt-1 fs-5 text-decoration-none text-black">Perfil de '.$_SESSION['user']['name'] .'</a>
         </li>
         <li class="list-group-item list-group-item-action"><a href="./../users/trash.php" class="fs-5 text-decoration-none text-black">Papelera</a></li>
         <li class="list-group-item list-group-item-action"><a href="./../users/contact.php" class="fs-5 text-decoration-none text-black">Contacto</a></li>
@@ -13,5 +13,5 @@ echo'
         <li class="list-group-item list-group-item-action"><a href="../../controllers/exit.php" class="fs-5 text-decoration-none text-black">Cerrar sesión</a></li>
         <li class="list-group-item list-group-item-action d-flex justify-content-center align-items-center"><i class="fs-4 la-1x las la-times icon__menu--close"></i></li>
     </ul>
-</section>';
+</nav>';
 ?>
